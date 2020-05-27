ce36687edc4361ab15dc313fee7ce4ffec1d24db
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-27 06:14:00 -0400

    Package updates

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,7 @@ module ReactMovieBrowserBackend
       g.test_framework nil
     end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+ 
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
